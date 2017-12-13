ceb74a8e3a2336d652b160d7e552698a709c0961
Jacob Edman <jedman42@gmail.com>
2017-12-12 16:31:43 -0800

    batch sounding files

--- utils/run_soundings.sh
+++ utils/run_soundings.sh
@@ -13,10 +13,21 @@ do
   echo $filename
   python sounding_to_rfm.py ../data/soundings/dry/$filename ../projects/atm/dry_co2$new.atm ../projects/outlevs.lev
   sed -i '' "s/dry_co2${old}/dry_co2${new}/" ../projects/rfm.drv
-  old=$new
-  cd ../projects/
-  ./rfm
-  cd ../utils/
-  python rfm_to_netcdf.py ../projects/out/ ../projects/ncout/dry_co2${new}.nc  
-done
+  old=$new 
+  
+  FILE=../projects/ncout/dry_co2${new}.nc
+  if [ ! -f $FILE ]; then
+      echo "File $FILE does not exist."
+      cd ../projects/
+      ./rfm
+      cd ../utils/
+      python rfm_to_netcdf.py ../projects/out/ ../projects/ncout/dry_co2${new}.nc  
+  
+  else
+      echo "File $FILE exists, continuing"
+      continue
+  fi
 
+done
+# reset driver file
+sed -i '' "s/dry_co2${old}/dry_co2_blah/" ../projects/rfm.drv
